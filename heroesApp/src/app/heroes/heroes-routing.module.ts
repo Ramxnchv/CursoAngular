40220672d20ed74568988e3441cd6b33b85c12df
Ramón Rosa Pérez <ramonros@ucm.es>
2023-08-25 18:17:30 +0200

    guards en heroesApp y estructura formsApp

--- heroesApp/src/app/heroes/heroes-routing.module.ts
+++ heroesApp/src/app/heroes/heroes-routing.module.ts
@@ -6,6 +6,8 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 
+
+// localhost:4200/heroes
 const routes: Routes = [
   {
     path: '',
@@ -16,7 +18,7 @@ const routes: Routes = [
       { path: 'edit/:id', component: NewPageComponent },
       { path: 'list', component: ListPageComponent },
       { path: ':id', component: HeroPageComponent },
-      { path: '**', redirectTo: 'list' }
+      { path: '**', redirectTo: 'list' },
     ]
   }
 ];
